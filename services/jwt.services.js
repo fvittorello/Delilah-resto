68778a173cce7a90e2f4ee686959317379d0ef3f
Fer Vittorello <fvittorello@gmail.com>
2020-09-03 05:35:10 -0300

    fix(jwt.services) change enviroment variable

--- services/jwt.services.js
+++ services/jwt.services.js
@@ -1,17 +1,16 @@
 const jwt = require('jsonwebtoken');
 
-const generateToken = (info) => {
-	const token = jwt.sign(info, process.env.JWT_SIGNATURE, {
-		expiresIn: '1h',
-	});
+function generateToken(info) {
+	const token = jwt.sign(info, process.env.TOKEN_SECRET);
+	console.log(`token generado ${token}`);
 	return token;
-};
+}
 
 async function validateToken(req, res, next) {
 	const token = req.headers.authorization.split(' ')[1];
 
 	try {
-		const validation = jwt.verify(token, process.env.JWT_SIGNATURE);
+		const validation = jwt.verify(token, process.env.TOKEN_SECRET);
 		next();
 	} catch (err) {
 		res.status(401).send('Usuario sin autorización o token expirado');
