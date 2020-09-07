d9a60772cc5464cd7e6b2e6b7e32d49c9955cdeb
Fer Vittorello <fvittorello@gmail.com>
2020-09-07 05:37:17 -0300

    feat(orders.js) add order get, get id, post, put and delete endpoints

--- routes/orders.js
+++ routes/orders.js
@@ -2,28 +2,35 @@ const express = require('express');
 const router = express.Router();
 const sequelize = require('../db');
 const { validateToken } = require('../services/jwt.services');
+const { validateOrderId, validateProductStatus } = require('../services/db.services');
 
 router.get('/', validateToken, async (req, res) => {
 	try {
 		const { user_id, is_admin, is_disabled } = req.token_info;
 
 		if (is_disabled) {
-			res.status(403).json('Tu usuario fue desabilitado y no tenes acceso a los pedidos');
+			res.status(403).json('Tu cuenta fue desabilitada y no tiene acceso a los pedidos');
 		}
 
 		if (is_admin) {
-			const pedidos = await sequelize.query('SELECT * FROM orders', { type: sequelize.QueryTypes.SELECT });
+			const pedidos = await sequelize.query(
+				'SELECT user_id, orders.order_id, orders_has_products.product_id, product_qty, unit_price, orders.created_at, order_status.status, payment_method.payment_method, order_description, title FROM orders_has_products INNER JOIN orders ON orders_has_products.order_id = orders.order_id INNER JOIN products ON orders_has_products.product_id = products.product_id INNER JOIN order_status ON orders.status = order_status.status_id INNER JOIN payment_method ON orders.payment_method = payment_method.payment_id',
+				{ type: sequelize.QueryTypes.SELECT }
+			);
 
 			res.status(200).json(pedidos);
 		} else {
-			const pedidos = await sequelize.query('SELECT * FROM orders WHERE user_id = :user_id', {
-				replacements: {
-					user_id,
-				},
-				type: sequelize.QueryTypes.SELECT,
-			});
+			const pedidos = await sequelize.query(
+				'SELECT user_id, orders.order_id, orders_has_products.product_id, product_qty, unit_price, orders.created_at, order_status.status, payment_method.payment_method, order_description, title FROM orders_has_products INNER JOIN orders ON orders_has_products.order_id = orders.order_id INNER JOIN products ON orders_has_products.product_id = products.product_id INNER JOIN order_status ON orders.status = order_status.status_id INNER JOIN payment_method ON orders.payment_method = payment_method.payment_id WHERE user_id = :user_id',
+				{
+					replacements: {
+						user_id,
+					},
+					type: sequelize.QueryTypes.SELECT,
+				}
+			);
 
-			res.status(200).sejsonnd(pedidos);
+			res.status(200).json(pedidos);
 		}
 	} catch (err) {
 		console.log(err);
@@ -31,35 +38,103 @@ router.get('/', validateToken, async (req, res) => {
 	}
 });
 
-router.post('/', validateToken, async (req, res) => {
+router.get('/:id', validateToken, validateOrderId, async (req, res) => {
 	try {
 		const { user_id, is_admin, is_disabled } = req.token_info;
 
 		if (is_disabled) {
-			res.status(403).json('Tu usuario se encuentra desabilitado y no puede hacer pedidos');
-		} else {
-			const { user_id, payment_type, order_description } = req.body;
+			res.status(403).json('Tu cuenta fue desabilitada y no tiene acceso a los pedidos');
+		}
 
-			const nuevoPedido = await sequelize.query(
-				'INSERT INTO orders (user_id, payment_type, order_description) VALUES (:user_id, :payment_type, :order_description)',
+		if (is_admin) {
+			const pedidos = await sequelize.query(
+				'SELECT user_id, orders.order_id, orders_has_products.product_id, product_qty, unit_price, orders.created_at, order_status.status, payment_method.payment_method, order_description, title FROM orders_has_products INNER JOIN orders ON orders_has_products.order_id = orders.order_id INNER JOIN products ON orders_has_products.product_id = products.product_id INNER JOIN order_status ON orders.status = order_status.status_id INNER JOIN payment_method ON orders.payment_method = payment_method.payment_id WHERE orders.order_id = :order_id',
+				{
+					replacements: {
+						order_id: req.params.id,
+					},
+					type: sequelize.QueryTypes.SELECT,
+				}
+			);
+
+			res.status(200).json(pedidos);
+		} else {
+			const pedidos = await sequelize.query(
+				'SELECT user_id, orders.order_id, orders_has_products.product_id, product_qty, unit_price, orders.created_at, order_status.status, payment_method.payment_method, order_description, title FROM orders_has_products INNER JOIN orders ON orders_has_products.order_id = orders.order_id INNER JOIN products ON orders_has_products.product_id = products.product_id INNER JOIN order_status ON orders.status = order_status.status_id INNER JOIN payment_method ON orders.payment_method = payment_method.payment_id WHERE user_id = :user_id AND orders.order_id = :order_id',
 				{
 					replacements: {
 						user_id,
-						payment_type,
+						order_id: req.params.id,
+					},
+					type: sequelize.QueryTypes.SELECT,
+				}
+			);
+
+			if (!pedidos.length) {
+				res.status(404).json(`No se encontraron pedidos con el id = ${req.params.id}`);
+			}
+
+			res.status(200).json(pedidos);
+		}
+	} catch (err) {
+		console.log(err);
+		res.status(500).json('Algo salio mal y no se pudieron traer los pedidos');
+	}
+});
+
+router.post('/', validateToken, validateProductStatus, async (req, res) => {
+	try {
+		const { user_id, is_disabled } = req.token_info;
+
+		if (is_disabled) {
+			res.status(403).json('Tu cuenta se encuentra desabilitada y no puede hacer nuevos pedidos');
+		} else {
+			const { payment_method, products, order_description = null } = req.body;
+
+			const newOrder = await sequelize.query(
+				'INSERT INTO orders (user_id, payment_method, order_description) VALUES (:user_id, :payment_method, :order_description)',
+				{
+					replacements: {
+						user_id,
+						payment_method,
 						order_description,
 					},
 				}
 			);
+
+			console.log(`El valor de newOrder es = ${newOrder[0]}`);
+
+			const producsOnOrder = products.forEach(async (product) => {
+				const prod_price = await sequelize.query(`SELECT price FROM products WHERE product_id = :product_id`, {
+					replacements: {
+						product_id: product.id,
+					},
+					type: sequelize.QueryTypes.SELECT,
+				});
+
+				const insert_prod = await sequelize.query(
+					'INSERT INTO orders_has_products (order_id, product_id, product_qty, unit_price) VALUES (:order_id, :product_id, :product_qty, :unit_price)',
+					{
+						replacements: {
+							order_id: newOrder[0],
+							product_id: product.id,
+							product_qty: product.qty,
+							unit_price: prod_price[0].price,
+						},
+					}
+				);
+			});
 
 			res.status(201).json('El pedido se ingreso con exito!');
 		}
 	} catch (err) {
 		console.log(err);
 		res.status(500).json('Algo salio mal, no se pudo ingresar el pedido');
+		next(err);
 	}
 });
 
-router.patch('/:id', validateToken, async (req, res) => {
+router.put('/:id', validateToken, validateOrderId, async (req, res) => {
 	try {
 		const { is_admin, is_disabled } = req.token_info;
 
@@ -87,6 +162,23 @@ router.patch('/:id', validateToken, async (req, res) => {
 	} catch (err) {
 		console.log(err);
 		res.status(500).json('Algo salio mal, no se pudo modificar el pedido');
+	}
+});
+
+router.delete('/:id', validateToken, validateOrderId, async (req, res) => {
+	try {
+		const { is_admin, is_disabled } = req.token_info;
+
+		if (is_admin && !is_disabled) {
+			const cancelOrder = await sequelize.query(`UPDATE orders SET status = 6 WHERE order_id = ${req.params.id}`);
+
+			res.status(201).json(`Se ha cancelado la orden con id = ${req.params.id}`);
+		} else {
+			res.status(403).json('Tu usuario se encuentra desabilitado o no tiene permisos para modificar ordenes.');
+		}
+	} catch (err) {
+		console.log(err);
+		res.status(500).json('Algo salio mal, no se pudo cancelar la orden');
 	}
 });
 
